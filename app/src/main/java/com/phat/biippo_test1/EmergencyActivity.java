a50a0f7697a1dfb4f892c158639c885b6b2d4a71
phatv4 <phattv41@gmail.com>
2015-02-19 21:35:04 -0800

    Initial commit

---
+++ app/src/main/java/com/phat/biippo_test1/EmergencyActivity.java
@@ -0,0 +1,82 @@
+package com.phat.biippo_test1;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+public class EmergencyActivity extends ActionBarActivity implements View.OnClickListener{
+
+    ImageButton button_jumpstart, button_fluidleak, button_warninglights;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency);
+
+        button_jumpstart = (ImageButton)findViewById(R.id.button_jumpstart);
+        button_jumpstart.setOnClickListener(this);
+        button_fluidleak = (ImageButton)findViewById(R.id.button_fluidleak);
+        button_fluidleak.setOnClickListener(this);
+        button_warninglights = (ImageButton)findViewById(R.id.button_warninglights);
+        button_warninglights.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_emergency, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void jumpstart_click()
+    {
+        startActivity(new Intent("com.phat.jumpstart"));
+    }
+
+    private void fluidleak_click()
+    {
+        startActivity(new Intent("com.phat.fluidleak"));
+    }
+
+    private void warninglights_click()
+    {
+        startActivity(new Intent("com.phat.warninglights"));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.button_jumpstart:
+                jumpstart_click();
+                break;
+            case R.id.button_fluidleak:
+                fluidleak_click();
+                break;
+            case R.id.button_warninglights:
+                warninglights_click();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/phat/biippo_test1/JumpStartActivity.java
@@ -0,0 +1,82 @@
+package com.phat.biippo_test1;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+public class JumpStartActivity extends ActionBarActivity implements View.OnClickListener{
+
+    ImageButton button_video, button_pictures, button_written;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jump_start);
+
+        button_video = (ImageButton)findViewById(R.id.button_fluidleak_video);
+        button_video.setOnClickListener(this);
+        button_pictures = (ImageButton)findViewById(R.id.button_fluidleak_pictures);
+        button_pictures.setOnClickListener(this);
+        button_written = (ImageButton)findViewById(R.id.button_fluidleak_written);
+        button_written.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_jump_start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void video_click()
+    {
+        startActivity(new Intent("com.phat.jumpstartvideo"));
+    }
+
+    private void pictures_click()
+    {
+        startActivity(new Intent("com.phat.jumpstartpictures"));
+    }
+
+    private void written_click()
+    {
+        startActivity(new Intent("com.phat.jumpstartwritten"));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.button_fluidleak_video:
+                video_click();
+                break;
+            case R.id.button_fluidleak_pictures:
+                pictures_click();
+                break;
+            case R.id.button_fluidleak_written:
+                written_click();
+                break;
+        }
+    }
+}
