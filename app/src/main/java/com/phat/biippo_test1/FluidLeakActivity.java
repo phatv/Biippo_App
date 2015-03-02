fa7a1af2d07cfed723deccfdba1d2f0eaa531d5c
phatv4 <phattv41@gmail.com>
2015-03-01 21:22:25 -0800

    Added jump start video

--- app/src/main/java/com/phat/biippo_test1/FluidLeakActivity.java
+++ app/src/main/java/com/phat/biippo_test1/FluidLeakActivity.java
@@ -3,8 +3,6 @@ package com.phat.biippo_test1;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
